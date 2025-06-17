b244a56c44811297a9f0abc09725bd022b34541a
Katya Weicht <katya.weicht@gmail.com>
2025-06-16 23:27:28 -0600

    Create while loop and if statement

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,23 @@
 import random
 
 secret_number = random.randint(1, 10)
+
+guesses_left = 5
+
+user_guess = 0
+
+while guesses_left > 0:
+  user_guess = int(input("Guess a number between 1 and 10: "))
+
+  if user_guess == secret_number:
+    print("Congratulations! You guessed the number!")
+    break
+  elif guesses_left == 1 and user_guess != secret_number:
+    print(f"Incorrect. Game Over! the secret number was {secret_number}")
+  elif user_guess > secret_number:
+    print("Too high! Try again.")
+  else:
+    print("Too low! try again.")
+  
+  guesses_left -= 1
+  
